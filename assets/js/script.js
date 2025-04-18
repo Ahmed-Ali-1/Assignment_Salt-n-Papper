49b95a0120ea2aea95a7b0d838023539f467d5ad
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-04-19 02:52:44 +0500

    Project

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+// owl-slider
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+    })
