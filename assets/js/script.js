21920f90d1a4df6e3938ff544fd4317a04a7869f
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-06-30 17:37:58 +0500

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,44 @@
-
 // owl-slider
 
-$('.insta-custom-section').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:2
-        },
-        400:{
-            items:3
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:5
-        }
-    }
-    })
+$(".insta-custom-section").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  center: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    400: {
+      items: 3.5,
+    },
+    750: {
+      items: 4,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
 
-$('.hello').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        400:{
-            items:1
-        },
-        750:{
-            items:2
-        },
-        1000:{
-            items:3
-        },
-        1200:{
-            items:4
-        }
-    }
-    })
+$(".display_resp_show").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: false,
+  center: true,
+  
+  responsive: {
+    0: {
+      items: 1.4,
+    },
+    500: {
+      items: 2,
+    },
+    750: {
+      items: 3.5,
+    },
+   
+  },
+});
